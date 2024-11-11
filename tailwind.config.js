4ad8eee5462d41d769c21058effa98a6239bf7fd
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-11 12:15:42 +0600

    add: navbar section

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import daisyui from "daisyui"
+
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+  daisyui:{
+    themes:['light']
+  }
+}
+
+export default config
